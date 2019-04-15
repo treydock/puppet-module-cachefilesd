93a14c54fe67c53aff84b8d8a66dece7de6f52b4
Trey Dockendorf <tdockendorf@osc.edu>
2019-04-15 14:17:58 -0400

    Make all resources optional

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,10 +5,16 @@
 # @example
 #   include cachefilesd
 #
+# @param manage_package
+#   Boolean that determines if package resource is managed.
 # @param package_name
 #   Package name for cachefilesd
 # @param package_ensure
 #   Package ensure property
+# @param manage_config
+#   Boolean that determines if config is managed.
+# @param config_path
+#   Path to cachefilesd.conf
 # @param manage_dir
 #   Booleans that determines if `dir` resource is managed.
 # @param dir
@@ -35,6 +41,8 @@
 #   cachefilesd `nocull` config option
 # @param resume_thresholds
 #   cachefilesd `resume_thresholds` config option
+# @param manage_service
+#   Boolean that determines if cachefilesd service is managed.
 # @param service_name
 #   cachefilesd service name
 # @param service_ensure
@@ -42,8 +50,11 @@
 # @param service_enable
 #   cachefilesd service enable property
 class cachefilesd (
+  Boolean $manage_package = true,
   String[1] $package_name = 'cachefilesd',
   String[1] $package_ensure = 'installed',
+  Boolean $manage_config = true,
+  Stdlib::Absolutepath $config_path = '/etc/cachefilesd.conf',
   Boolean $manage_dir = true,
   Stdlib::Absolutepath $dir = '/var/cache/fscache',
   Variant[String[1], Boolean] $cache_tag = 'CacheFiles',
@@ -57,42 +68,62 @@ class cachefilesd (
   Integer[12,20] $culltable = 12,
   Boolean $nocull = false,
   Optional[String[1]] $resume_thresholds = undef,
+  Boolean $manage_service = true,
   String[1] $service_name = 'cachefilesd',
   String[1] $service_ensure = 'running',
   Boolean $service_enable = true,
 ) {
 
-  package { 'cachefilesd':
-    ensure => $package_ensure,
-    name   => $package_name,
+  if $manage_package {
+    package { 'cachefilesd':
+      ensure => $package_ensure,
+      name   => $package_name,
+    }
+    if $manage_dir {
+      Package['cachefilesd'] -> File[$dir]
+    }
+    if $manage_config {
+      Package['cachefilesd'] -> File['cachefilesd.conf']
+    }
+    if $manage_service {
+      Package['cachefilesd'] ~> Service['cachefilesd']
+    }
   }
 
   if $manage_dir {
     file { $dir:
-      ensure  => 'directory',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755',
-      require => Package['cachefilesd'],
-      notify  => Service['cachefilesd'],
+      ensure => 'directory',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+    }
+    if $manage_service {
+      File[$dir] ~> Service['cachefilesd']
     }
   }
 
-  file { '/etc/cachefilesd.conf':
-    ensure  => 'file',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('cachefilesd/cachefilesd.conf.erb'),
-    require => Package['cachefilesd'],
-    notify  => Service['cachefilesd'],
+  if $manage_config {
+    file { 'cachefilesd.conf':
+      ensure  => 'file',
+      path    => $config_path,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('cachefilesd/cachefilesd.conf.erb'),
+      require => Package['cachefilesd'],
+      notify  => Service['cachefilesd'],
+    }
+    if $manage_service {
+      File['cachefilesd.conf'] ~> Service['cachefilesd']
+    }
   }
 
-  service { 'cachefilesd':
-    ensure  => $service_ensure,
-    enable  => $service_enable,
-    name    => $service_name,
-    require => Package['cachefilesd'],
+  if $manage_service {
+    service { 'cachefilesd':
+      ensure  => $service_ensure,
+      enable  => $service_enable,
+      name    => $service_name,
+    }
   }
 
 }
